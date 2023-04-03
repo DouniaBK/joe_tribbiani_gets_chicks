7e18cf298dc5c39edee7a95a870431fc36d17505
DouniaBK <douniazedbacha@gmail.com>
2023-04-03 10:29:33 +0000

    Add print_field function to set the field measurments and border

--- run.py
+++ run.py
@@ -1,15 +1,23 @@
 
 """ 
 setting the field's hight and width
-setting the field's end zone
+setting the field's end zone and borders
 """
+
+
 def print_field():
-   for cell in CELLS:
-    print(cell)
+    for cell in CELLS:
+        if cell[0] in (0, FIELD_WIDTH - 1) or cell[1] in (0, FIELD_HEIGHT - 1):
+            print('#', end='')
+        else: 
+            print(' ', end='')
+        if cell[0] == FIELD_HEIGHT - 1:
+            print('')
+
 
 FIELD_WIDTH = 32
 FIELD_HEIGHT = 36
-CELLS = [(col,row) for row in range(FIELD_HEIGHT) for col in range(FIELD_WIDTH)]
+CELLS = [(col, row) for row in range(FIELD_HEIGHT) for col in range(FIELD_WIDTH)]
 
 print_field()
 
