d0ccf6fe237d04a54322978b8092455068066f86
DouniaBK <douniazedbacha@gmail.com>
2023-04-04 15:01:50 +0000

    add chick_collision and new_position to update the snakes position

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 
 from pytimedinput import timedInput
 import os
+from random import randint
+
 """ 
 setting the field's hight and width
 setting the field's end zone and borders
@@ -30,14 +32,28 @@ def move_joe():
     joes_body.pop(-1)
 
 
+def chick_collision():
+    global chick_position, eaten
+
+    if chick_position[0] == joes_body:
+        chick_position = new_position()
+        eaten = True
+
+
+def new_position():
+    col = randint(1, FIELD_WIDTH - 2)
+    row = randint(1, FIELD_HEIGHT - 2)
+    while (col, row) in joes_body:
+        col = randint(1, FIELD_WIDTH -2)
+        row = randint(1, FIELD_HEIGHT -2)
+    return (col, row)
+
 # Field settings
 FIELD_WIDTH = 32
 FIELD_HEIGHT = 36
 CELLS = [(col, row) for row in range(FIELD_HEIGHT) for col in range(FIELD_WIDTH)]
 
 
-# The position of the chick or food
-chick_position = (5, 10)
 """
 The body of the snake a.k.a Joe (col, row // 2)
 the body consists of three parts which are the three items in the list below
@@ -50,7 +66,8 @@ joes_body = [(5, FIELD_HEIGHT // 2),(4, FIELD_HEIGHT // 2),(3, FIELD_HEIGHT // 2
 DIRECTIONS = {'left': (-1,0),'right': (1,0),'up': (0,-1,),'down': (0,1)}
 #start of direction
 direction = DIRECTIONS['right']
-
+# The position of the chick or food
+chick_position = new_position()
 
 """
 limit the speed of the while loop to 0.3ms
@@ -60,8 +77,12 @@ without the while loop the game will only run once
 while True:
     # clear field in terminal
 	os.system('cls')
+
+
     # draw the game field
-    print_field ()
+    print_field()
+
+
     # get text that was entered and timed input
 	txt,_ = timedInput('',timeout = 0.3)
     match txt:
@@ -77,6 +98,7 @@ while True:
 
     #update game elements position
     move_joe()
+    chick_collision()
     
 
 
